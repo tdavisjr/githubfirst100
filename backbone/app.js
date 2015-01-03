cfcb20ad035d112a054de327bdede1e0c287a780
Tyrone Davis <tdavisjr@gmail.com>
2015-01-03 13:00:37 -0500

    minor refactor to pass collection to view

--- backbone/app.js
+++ backbone/app.js
@@ -1,66 +1,66 @@
-var app = {};
+    var app = {};
 
-(function ($) {
-    "use scrict";
+    (function ($) {
+        "use scrict";
 
-    var GitHubUser = Backbone.Model.extend({});
+        var GitHubUser = Backbone.Model.extend({});
 
-    var GitHubUsers = Backbone.Collection.extend({
-        model: GitHubUser,
-        url: 'https://api.github.com/users'
-    });
-
-    app.users = new GitHubUsers();
-
-    var UserView = Backbone.View.extend({
-        tagName: 'div',
-        className: 'col-md-1',
-
-        template: _.template($('#user-row').html()),
-
-        render: function () {
-            this.$el.html(this.template(this.model.toJSON()));
-
-            return this;
-        }
-
-    });
+        var GitHubUsers = Backbone.Collection.extend({
+            model: GitHubUser,
+            url: 'https://api.github.com/users'
+        });
 
 
-    var GitHubApp = Backbone.View.extend({
-        el: '#app',
+        var UserView = Backbone.View.extend({
+            tagName: 'div',
+            className: 'col-md-1',
 
-        events: {
+            template: _.template($('#user-row').html()),
 
-        },
+            render: function () {
+                this.$el.html(this.template(this.model.toJSON()));
 
-        initialize: function () {
-            this.$list = $('.row');
+                return this;
+            }
 
-            this.listenTo(app.users, 'add', this.renderUser);
-            this.listenTo(app.users, 'sync', this._onFetchComplete);
+        });
 
-            app.users.fetch();
-        },
-        
-        _onFetchComplete: function(){
-            this.$('.bg-info').remove();
-        },
 
-        renderUser: function (user) {
-            var aUser = new UserView({
-                model: user
-            });
-            var html = aUser.render().el;
+        var GitHubApp = Backbone.View.extend({
+            el: '#app',
 
-            this.$list.append(html);
-        },
-    });
+            events: {
 
-    app.init = function () {
-        new GitHubApp();
-    };
+            },
 
-}(jQuery));
+            initialize: function () {
+                this.$list = $('.row');
 
-app.init();
+                console.log(this.collection)
+                this.listenTo(this.collection, 'add', this.renderUser);
+                this.listenTo(this.collection, 'sync', this._onFetchComplete);
+
+                this.collection.fetch();
+            },
+
+            _onFetchComplete: function () {
+                this.$('.bg-info').remove();
+            },
+
+            renderUser: function (user) {
+                var aUser = new UserView({
+                    model: user
+                });
+                var html = aUser.render().el;
+
+                this.$list.append(html);
+            },
+        });
+
+        app.init = function () {
+            new GitHubApp({collection: new GitHubUsers()});
+        };
+
+    }(jQuery));
+
+    app.init();
