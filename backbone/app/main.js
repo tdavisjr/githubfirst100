0688f31b71fc6ddbffcdf7eed02089d92a962242
Tyrone Davis <tdavisjr@gmail.com>
2016-01-03 13:33:05 -0500

    removed unnecessary shim

--- backbone/app/main.js
+++ backbone/app/main.js
@@ -3,13 +3,7 @@ require.config({
         jquery: '../vendor/jquery/dist/jquery.min',
         backbone: '../vendor/backbone/backbone',
         underscore: '../vendor/underscore/underscore'  
-    },
-    shim: {
-        'backbone': {
-          deps: ['underscore', 'jquery']  
-        }
     }
-
 });
 
 require(['jquery', 'appView', 'users'], function($, AppView, Users){
