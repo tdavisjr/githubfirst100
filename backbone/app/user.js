40e0580abc062758398bc3e8e3cf034a3f861cf6
Tyrone Davis <t_davisjr@hotmail.com>
2016-01-02 22:27:17 -0500

    remove user.js - not needed

--- backbone/app/user.js
+++
@@ -1,5 +0,0 @@
-define(function (require, exports, module) {
-    var Backbone = require("backbone");
-
-    module.exports = Backbone.Model.extend({});
-});
--- backbone/app/users.js
+++ backbone/app/users.js
@@ -1,10 +1,7 @@
 define(function(require, exports, module){
     var Backbone = require("backbone");
-    var $ = require("jquery");
-    var User = require("user");
 
     module.exports = Backbone.Collection.extend({
-        model: User,
         url: 'https://api.github.com/users'
     });
     
