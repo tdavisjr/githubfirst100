ca389e70c5cac3dbf70df91072509fe37120dcee
Tyrone Davis <tdavisjr@gmail.com>
2016-06-24 22:37:13 -0400

    minor refactor

--- vue/app/main.js
+++ vue/app/main.js
@@ -1,54 +1,38 @@
-require.config({
-  paths: {
-    fetch: '../vendor/fetch/fetch',
-    vue: '../vendor/vue/dist/vue.min',
-  },
-  shim: {
-    "fetch": {
-      exports: 'fetch'
-    }
-  }
-});
-
 var modules = [
-  'vue',
-  'fetch',
-  'pageHeaderView',
-  'loaderView',
-  'usersView'
+    'vue',
+    'fetch',
+    'pageHeaderView',
+    'loaderView',
+    'usersView'
 
 ];
 
 require(modules, function(Vue, fetch, PageHeaderView, LoaderView, UsersView) {
 
-  Vue.component('page-header', PageHeaderView);
-  Vue.component('loader', LoaderView);
-  Vue.component('users-list', UsersView);
+    Vue.component('page-header', PageHeaderView);
+    Vue.component('loader', LoaderView);
+    Vue.component('users-list', UsersView);
 
-  var vm = new Vue({
-    el: '#app',
-    data: {
-      isLoading: true,
-      users: []
-    },
-    created: function() {
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            isLoading: true,
+            users: []
+        },
 
-    },
-    ready: function() {
-      var self = this;
-
-      fetch('https://api.github.com/users')
-        .then(function(response) {
-          return response.json()
-        })
-        .then(function(data) {
-          self.users = data;
-          self.isLoading = false;
-        })
-        .catch(function(ex) {
-          console.log('parsing failed', ex)
-        });
-    }
-  });
+        ready: function() {
+            fetch('https://api.github.com/users')
+                .then(function(response) {
+                    return response.json()
+                })
+                .then(function(data) {
+                    this.users = data;
+                    this.isLoading = false;
+                }.bind(this))
+                .catch(function(ex) {
+                    console.log('parsing failed', ex)
+                });
+        }
+    });
 
 });
